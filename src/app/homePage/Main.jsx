fd95a071205139d1b8365498301aa15e00bfcab3
yizhang199 <yizhang19801001@gmail.com>
2019-04-26 13:21:01 +1000

    setup grid view

--- src/app/homePage/Main.jsx
+++ src/app/homePage/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { OrderList } from "./components";
+import { OrderList, OrderDetail, ControlPannel } from "./components";
 export default () => {
   return (
     <div className="home-page">
       <OrderList />
+      <OrderDetail />
+      <ControlPannel />
     </div>
   );
 };
