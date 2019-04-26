84511f17057d65d5907d1a5a8f2df1c8b27fb909
yizhang199 <yizhang19801001@gmail.com>
2019-04-26 10:26:30 +1000

    improve ui styling: login form

--- src/app/routes.js
+++ src/app/routes.js
@@ -6,7 +6,7 @@ import { Login } from "./auth";
 export default () => {
   return (
     <Router history={history}>
-      <Route exact path={`${process.env.PUBLIC_URL}/`} component={HomePage} />
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={Login} />
       <Route path={`${process.env.PUBLIC_URL}/login`} component={Login} />
       <Route path={`${process.env.PUBLIC_URL}/home`} component={HomePage} />
     </Router>
